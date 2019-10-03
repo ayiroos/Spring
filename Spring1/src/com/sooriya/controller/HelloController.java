b654d98e1d3f367c1b4b041433d65432efa2a1ca
ayiroos <onesooriya@gmail.com>
2019-10-03 16:05:23 +0530

    used @RequestParam annotation, collected data from user from html form and navigate to other form

--- Spring1/src/com/sooriya/controller/HelloController.java
+++ Spring1/src/com/sooriya/controller/HelloController.java
@@ -4,21 +4,30 @@ package com.sooriya.controller;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
 
-	@RequestMapping("/welcome/{country}/{name}")
-	protected ModelAndView myApp(@PathVariable Map<String, String> vars) {
+	@RequestMapping (value = "/submitForm", method = RequestMethod.GET)
+	public ModelAndView submitFormDetails () {
 		
-		String country = vars.get("country");
-		String name = vars.get("name");
-		ModelAndView modelAndView = new ModelAndView("hello");
-		modelAndView.addObject("welcome", "Hey "+ name + "You are from "+ country);
+		ModelAndView modelAndView = new ModelAndView ("SubmitForm");
+		return modelAndView;
+	}
+	
+	@RequestMapping (value = "/getDetails", method = RequestMethod.POST)
+	public ModelAndView getFormDetails (@RequestParam Map<String, String> vars) {
+		
+		ModelAndView modelAndView = new ModelAndView("GetForm");
+		String hobby = vars.get("studentHobby");
+		String name = vars.get("studentName");
+		
+		modelAndView.addObject("formData", "Hey "+ name + "You are interested in "+ hobby);
 		return modelAndView;
 	}
 
